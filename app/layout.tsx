bee9817666c4ce2557b1fac3db9c5a4857d19c37
codeblue12 <yasir.codeblue@gmail.com>
2024-08-22 16:05:07 +0500

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Yasir || Job Portal",
+  description: "Next Js Typescript Job Portal Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+
+      <body className={poppins.className}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return ( 
+    <>
+    <Button>Click me</Button>
+    </>
+   );
+}
+ 
+export default HomePage;
